334ae78d2f7aa408373cb2c2bbef70fb91960683
Lum <LumKitty@users.noreply.github.com>
2026-09-16 23:47:25 +0100

    Bump version to 1.0

--- VNyan-FollowCam/VNyan_Handlers.cs
+++ VNyan-FollowCam/VNyan_Handlers.cs
@@ -14,7 +14,7 @@ namespace VNyan_FollowCam {
 
     public class VNyan_Handlers : IVNyanPluginManifest, IButtonClickedHandler, ITriggerHandler {
         public string PluginName { get; } = "VNyan FollowCam";
-        public string Version { get; } = "0.9-beta";
+        public string Version { get; } = "1.0";
         public string Title => PluginName + " " + Version;
         public string Author { get; } = "LumKitty";
         public string Website { get; } = "https://lum.uk/";
